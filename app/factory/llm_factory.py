dee74e2eeec75b3a9648e4f2b07200942a8a5486
kurtyoon <akakertcobain@naver.com>
2025-03-07 04:41:31 +0900

    fix: dependency

--- app/factory/llm_factory.py
+++ app/factory/llm_factory.py
@@ -1,4 +1,4 @@
-from langchain_models.core.language_models.base import BaseLanguageModel
+from langchain_core.language_models.base import BaseLanguageModel
 from langchain_openai import ChatOpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 
